2b7a1932e1b75d235f40cfa764a55dfa2d5ee692
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-04-21 23:44:34 -0500

    update

--- MySQL/Usuarios y roles/Rol-administrador.sql
+++ MySQL/Usuarios y roles/Rol-administrador.sql
@@ -3,7 +3,7 @@ CREATE ROLE 'admin_role';
 
 -- Asignar todos los permisos al rol, menos los permisos de eliminación
 GRANT ALL PRIVILEGES ON `FabiaNatura`.* TO 'admin_role' WITH GRANT OPTION;
-REVOKE DELETE, DROP ON `FabiaNatura`.* FROM 'admin_role';
+REVOKE DELETE, CREATE, DROP ON `FabiaNatura`.* FROM 'admin_role';
 
 -- Demostracion
 CREATE USER 'admin1'@'localhost' IDENTIFIED BY 'tu_contraseña_segura';

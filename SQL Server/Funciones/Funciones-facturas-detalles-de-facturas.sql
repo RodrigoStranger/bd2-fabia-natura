64b2595558cd2b695877bdbaf3f8240d4f253547
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-04-24 11:20:14 -0500

    update

---
+++ SQL Server/Funciones/Funciones-facturas-detalles-de-facturas.sql
@@ -0,0 +1,20 @@
+USE FabiaNatura;
+GO
+
+CREATE FUNCTION Ventas.ObtenerUltimaFactura()
+RETURNS INT
+AS
+BEGIN
+    DECLARE @ultima_factura INT;
+
+    -- Obtener el código de la última factura creada (basado en la fecha de registro más reciente)
+    SELECT TOP 1 @ultima_factura = cod_factura
+    FROM Ventas.Facturas
+    ORDER BY fecha_registro DESC;
+
+    -- Devolver el código de la última factura
+    RETURN @ultima_factura;
+END;
+
+-- SELECT dbo.Ventas.ObtenerUltimaFactura() AS UltimaFactura;
+

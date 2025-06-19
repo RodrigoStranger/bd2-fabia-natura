77e38fdfcb1ab4b1f5f759ee85531607a6d7555b
Carlos Daniel Aguilar Chirinos <caguilarc@ulasalle.edu.pe>
2025-06-19 13:51:15 -0500

    update

--- SQL Server/Auditorias/Audit.sql
+++ SQL Server/Auditorias/Audit.sql
@@ -10,12 +10,12 @@ GO
 USE FabiaNatura;
 GO
 -- Crear la especificación de auditoría a nivel de base de datos
-CREATE DATABASE AUDIT SPECIFICATION AuditoriaFBJPerez
+CREATE DATABASE AUDIT SPECIFICATION Auditoriausuario01
 FOR SERVER AUDIT AuditoriaServidor
-ADD (SELECT, INSERT, UPDATE ON OBJECT::[Ventas].[Facturas] BY JPérez),
-ADD (SELECT, INSERT, UPDATE ON OBJECT::[Ventas].[Detalle_Facturas] BY JPérez),
-ADD (SELECT, INSERT, UPDATE ON OBJECT::[RecursosHumanos].[Clientes] BY JPérez),
-ADD (SELECT, INSERT, UPDATE ON OBJECT::[Inventario].[Productos] BY JPérez),
-ADD (SELECT, INSERT, UPDATE ON OBJECT::[Inventario].[Categorias] BY JPérez);
+ADD (SELECT, INSERT, UPDATE ON OBJECT::[Ventas].[Facturas] BY usuario01),
+ADD (SELECT, INSERT, UPDATE ON OBJECT::[Ventas].[Detalle_Facturas] BY usuario01),
+ADD (SELECT, INSERT, UPDATE ON OBJECT::[RecursosHumanos].[Clientes] BY usuario01),
+ADD (SELECT, INSERT, UPDATE ON OBJECT::[Inventario].[Productos] BY usuario01),
+ADD (SELECT, INSERT, UPDATE ON OBJECT::[Inventario].[Categorias] BY usuario01);
 GO
-ALTER DATABASE AUDIT SPECIFICATION AuditoriaFBJPerez WITH (STATE = ON);
+ALTER DATABASE AUDIT SPECIFICATION Auditoriausuario01 WITH (STATE = ON);
